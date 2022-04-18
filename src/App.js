b47c18aa1c430284b3da2abcb1fd37f704e85ab4
Drew Daniels <daniels_drew@protonmail.com>
2022-04-18 12:46:43 -0500

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDG2fLLEjnjdKsDsPTSVRiruK_FyisIcJ8",
+  authDomain: "drew-daniels-wheres-waldo.firebaseapp.com",
+  projectId: "drew-daniels-wheres-waldo",
+  storageBucket: "drew-daniels-wheres-waldo.appspot.com",
+  messagingSenderId: "455264246838",
+  appId: "1:455264246838:web:f027b4bcd76e2e7d448c97"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
