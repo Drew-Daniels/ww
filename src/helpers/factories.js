0d841ffd19d7b0dab16ee980409c93a2ea8617c0
Drew Daniels <daniels_drew@protonmail.com>
2022-04-25 21:43:32 -0500

    Installed jest and have begun working on unit tests.

--- src/helpers/factories.js
+++ src/helpers/factories.js
@@ -1,13 +1,37 @@
-const UserFactory = (name) => ({
-    name,
-  });
-  
-const GameFactory = (user_id, duration_sec) => ({
-    user_id,
-    duration_sec,
-})
+/**
+ * 
+ * @param {integer} id 
+ * @param {*} name 
+ * @returns User object
+ */
+export const UserFactory = (id, name) => {
+    if (!Number.isSafeInteger(id)) {
+        throw new Error('id must be an integer');
+    }
+    if (id < 1) {
+        throw new Error('id must be greater than 0');
+    }
+    return { 
+        id : id,
+        name : name ? String(name) : '',
+    };
+};
 
-export {
-    UserFactory,
-    GameFactory,
+/**
+ * 
+ * @param {integer} user_id 
+ * @param {integer} duration 
+ * @returns Game object
+ */
+export const GameFactory = (user_id, duration) => {
+    if (!Number.isSafeInteger(user_id)) {
+        throw new Error('user_id must be an integer');
+    }
+    if (user_id < 1) {
+        throw new Error('user_id must be 1 or greater');
+    }
+    if (duration < 0) {
+        throw new Error('duration must be 0 or greater')
+    }
+    return { user_id, duration };
 }
---
+++ src/helpers/factories.test.js
@@ -0,0 +1,32 @@
+import { UserFactory, GameFactory } from './factories';
+
+describe('UserFactory tests', () => {
+    describe('UserFactory id tests', () => {
+        it('Accepts an integer greater than 0 for id', () => {
+            // TODO: Find a way to just test for id property, value instead of including the name property, value as a caveat
+            expect(UserFactory(1)).toEqual({ id: 1, name: '' });
+        });
+        it('Does NOT accept a decimal for id', () => {
+            expect(UserFactory(1.1)).toThrow();
+        });
+        it('Does NOT accept a 0 for id', () => {
+            expect(UserFactory(0)).toThrow();
+        });
+        it('Does NOT accept a negative integer for id', () => {
+            expect(UserFactory(-1)).toThrow();
+        });
+        it('Does NOT accept no value being passed for id', () => {
+            expect(UserFactory()).toThrow();
+        });
+
+    })
+    describe('UserFactory name tests', () => {
+
+    })
+})
+
+
+describe('GameFactory tests', () => {
+    it('Accepts an integer as "user_id')
+    it('Accepts string as "user_id"')
+})
