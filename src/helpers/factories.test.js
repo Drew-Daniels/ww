8ab186fdcdfb176430072d077867c7e5943b4b85
Drew Daniels <daniels_drew@protonmail.com>
2022-04-26 08:16:23 -0500

    Refactored UserFactory tests to 'toMatchObject' expect method to be more modular.

--- src/helpers/factories.test.js
+++ src/helpers/factories.test.js
@@ -2,31 +2,55 @@ import { UserFactory, GameFactory } from './factories';
 
 describe('UserFactory tests', () => {
     describe('UserFactory id tests', () => {
-        it('Accepts an integer greater than 0 for id', () => {
-            // TODO: Find a way to just test for id property, value instead of including the name property, value as a caveat
-            expect(UserFactory(1)).toEqual({ id: 1, name: '' });
+        it('accepts an integer greater than 0 for id', () => {
+            expect(UserFactory(1)).toMatchObject({ id: 1 });
         });
-        it('Does NOT accept a decimal for id', () => {
+        it('does NOT accept a decimal for id', () => {
             expect(() => UserFactory(1.1)).toThrow();
         });
-        it('Does NOT accept a 0 for id', () => {
+        it('does NOT accept a 0 for id', () => {
             expect(() => UserFactory(0)).toThrow();
         });
-        it('Does NOT accept a negative integer for id', () => {
+        it('does NOT accept a negative integer for id', () => {
             expect(() => UserFactory(-1)).toThrow();
         });
-        it('Does NOT accept no value being passed for id', () => {
+        it('does NOT accept no value being passed for id', () => {
             expect(() => UserFactory()).toThrow();
         });
 
     });
-    // describe('UserFactory name tests', () => {
-
-    // });
+    describe('UserFactory name tests', () => {
+        it('accepts no arguments passed for name', () => {
+            expect(UserFactory(1)).toMatchObject({ name: '' });
+        });
+        it('accepts a blank string for name', () => {
+            expect(UserFactory(1, '')).toMatchObject({ name: '' });
+        });
+        it('accepts a passed-in string for name', () => {
+            expect(UserFactory(1, 'spam')).toMatchObject({ name: 'spam' });
+        });
+        it('accepts an integer for name', () => {
+            expect(UserFactory(1, 1)).toMatchObject({ name: '1' });
+        });
+        it('accepts a decimal for name', () => {
+            expect(UserFactory(1, 1.1)).toMatchObject({ name: '1.1' });
+        });
+        it('accepts symbols for name', () => {
+            expect(UserFactory(1, '#$%^&*@!~_+=?')).toMatchObject({ name: '#$%^&*@!~_+=?' });
+        });
+    });
 });
 
-
 // describe('GameFactory tests', () => {
-//     it('Accepts an integer as "user_id')
-//     it('Accepts string as "user_id"')
-// })
+//     describe('GameFactory "id" tests', () => {
+//         it('accepts an integer as "user_id', () => {
+//             expect(GameFactory(1, 30)).toEqual()
+//         });
+//         it('accepts string as "user_id"', () => {
+            
+//         });
+//     });
+//     describe('GameFactory "duration" tests', () => {
+        
+//     })
+// });
