ad9cdaae5a159e04ec3f2cf55fa9f292e5b1f3cc
Drew Daniels <daniels_drew@protonmail.com>
2022-04-26 08:00:19 -0500

    Finished creating UserFactory id tests.

--- src/helpers/factories.test.js
+++ src/helpers/factories.test.js
@@ -7,26 +7,26 @@ describe('UserFactory tests', () => {
             expect(UserFactory(1)).toEqual({ id: 1, name: '' });
         });
         it('Does NOT accept a decimal for id', () => {
-            expect(UserFactory(1.1)).toThrow();
+            expect(() => UserFactory(1.1)).toThrow();
         });
         it('Does NOT accept a 0 for id', () => {
-            expect(UserFactory(0)).toThrow();
+            expect(() => UserFactory(0)).toThrow();
         });
         it('Does NOT accept a negative integer for id', () => {
-            expect(UserFactory(-1)).toThrow();
+            expect(() => UserFactory(-1)).toThrow();
         });
         it('Does NOT accept no value being passed for id', () => {
-            expect(UserFactory()).toThrow();
+            expect(() => UserFactory()).toThrow();
         });
 
-    })
-    describe('UserFactory name tests', () => {
+    });
+    // describe('UserFactory name tests', () => {
 
-    })
-})
+    // });
+});
 
 
-describe('GameFactory tests', () => {
-    it('Accepts an integer as "user_id')
-    it('Accepts string as "user_id"')
-})
+// describe('GameFactory tests', () => {
+//     it('Accepts an integer as "user_id')
+//     it('Accepts string as "user_id"')
+// })
